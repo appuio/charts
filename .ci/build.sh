4a42c1123710a131b2d6eafa839e373c30592808
spiogit <spiogit@users.noreply.github.com>
2019-02-22 12:00:03 +0100

    HelmQA integration (#22)
    First iteration of a HelmQA script into the CI process

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,10 +1,52 @@
 #!/bin/bash
 set -e
 
+# set to one of the following values
+# - don't use HelmQA: (empty)
+# - use HelmQA in SaaS mode: 'http://helmqa-zhaw-prod-demos.appuioapp.ch/livecheck?repo'
+# - use HelmQA locally in Docker mode: 'http://127.0.0.1:5000/livecheck?repo'
+HELMQA_URL='http://helmqa-zhaw-prod-demos.appuioapp.ch/livecheck?repo'
 REPO_URL="${REPO_URL:-https://charts.appuio.ch}"
 echo "----> Deploying to $REPO_URL"
 
 GIT_REPO="$(git config remote.origin.url)"
+
+if [ -n "$HELMQA_URL" ]; then
+  did=
+  if [[ "$HELMQA_URL" =~ 127.0.0.1 ]]; then
+    docker pull docker.io/jszhaw/helmqa
+    did="$(docker run -d -p 127.0.0.1:5000:5000 docker.io/jszhaw/helmqa)"
+  fi
+
+  while true; do
+    sleep 1
+    tresponse="$(curl --connect-timeout 5 --max-time 60 $HELMQA_URL=$GIT_REPO)" || true
+    if [ -n "$tresponse" ]; then
+      break
+    fi
+    echo "Still spinning up HelmQA container image..."
+  done
+
+  if [[ $? != 28 ]]; then
+    tstatus=$(echo "$tresponse" | jq -r .status)
+
+    if [[ "$tstatus" == "fail" ]]; then
+      echo "HelmQA test failed. Check response"
+      echo "$tresponse"
+      exit 1
+    elif [[ "$tstatus" == "success" ]]; then
+      echo "HelmQA test succeeded. No issues found"
+    fi
+  else
+    echo "HelmQA connection failed. Timed out!"
+  fi
+
+  if [ -n "$did" ]; then
+    docker kill "$did"
+  fi
+else
+  echo "Skipping HelmQA test."
+fi
 
 tmp="$(mktemp -d)"
 echo "----> Working in $tmp"
