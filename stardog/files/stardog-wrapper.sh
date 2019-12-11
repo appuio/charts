b29dc9dfc36a9a16d3683e7e0fbb96be65947da6
Chris <github.account@chrigel.net>
2019-12-11 14:40:25 +0100

    Rework the script, add feature to use add roles
    Signed-off-by: Chris <github.account@chrigel.net>

--- stardog/files/stardog-wrapper.sh
+++ stardog/files/stardog-wrapper.sh
@@ -11,8 +11,8 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
-OPTIONS=ahn:p:dvg
-LONGOPTS=disable-user,set-admin-password,help,new-user-name:,new-user-pass-from:,new-database:,grant-user:,verbose
+OPTIONS=hv
+LONGOPTS=disable-user:,set-admin-password,help,ensure-user:,user-pass-from:,new-database:,ensure-role:,ensure-role-grants:,assign-role-to-user:,verbose
 
 # -regarding ! and PIPESTATUS see above
 # -temporarily store output to be able to check for errors
@@ -32,12 +32,17 @@ function usage {
 Usage: $(basename "$0") [OPTION]...
 
      --disable-user        USER                 Disable the given user.
-  -a,--set-admin-password                       Sets the admin password. Needs \$STARDOG_ADMIN_PW set as ENV var.
-  -n,--new-user-name       USER                 Adds a new user name with the given name. Needs -p.
-  -p,--new-user-pass-from  PATH                 When using --new-user-name, set a path to a file which contains the new user password.
-  -g,--grant-user          USER,ACTION,OBJECT   Grant an action for an object for a user. (e.g. peter,write,db:stardog)
-  -d,--new-database        VALUE                Creates a new database with the given name. Pass additional options after --, e.g. "--new-database stardog -- -o search.enabled=true"
-  -h,--help                                     Display this help.
+     --set-admin-password                       Sets the admin password. Needs \$STARDOG_ADMIN_PW set as ENV var.
+     --ensure-user         USER                 Ensures a user exists with the given name. Needs --user-pass-from flag.
+                                                Accepts additional options after -- (e.g. '-- -s' for superuser)
+     --user-pass-from      PATH                 When using --ensure-user, set a path to a file which contains the user password.
+     --ensure-role         ROLE                 Ensures a role exists with the given name.
+     --ensure-role-grants  ROLE,ACTION,OBJECT   Ensures an existing role has the given permissions and grant objects (e.g. role_name,write,db:*)
+     --assign-role-to-user ROLE,USER
+     --new-database        VALUE                Creates a new database with the given name. Pass additional options after --,
+                                                e.g. "--new-database stardog -- -o search.enabled=true"
+                                                Will skip existing databases.
+  -h,--help                                     Display this help and exit.
   -v,--verbose                                  Verbose mode.
 EOM
 	exit 2
@@ -50,8 +55,10 @@ disable_user=false
 set_admin_password=false
 verbose=false
 new_database=false
-new_user=false
-grant_user=false
+ensure_user=false
+ensure_role=false
+ensure_role_grants=false
+assign_role=false
 options=""
 
 # now enjoy the options in order and nicely split until we see --
@@ -60,48 +67,46 @@ while true; do
            --disable-user)
             disable_user=true
             user_name="${2}"
-            shift 2
-            ;;
-        -a|--set-admin-password)
+            shift 2 ;;
+          --set-admin-password)
             set_admin_password=true
-            shift
-            ;;
+            shift ;;
         -h|--help)
             usage
-            exit 1
-            ;;
-        -n|--new-user-name)
-            new_user=true
-            new_user_name="${2}"
-            shift 2
-            ;;
-        -p|--new-user-pass-from)
-            new_user_pass_from="${2}"
-            shift 2
-            ;;
-        -d|--new-database)
+            exit 1 ;;
+           --ensure-user)
+            ensure_user=true
+            user_name="${2}"
+            shift 2 ;;
+           --user-pass-from)
+            user_pass_from="${2}"
+            shift 2 ;;
+           --new-database)
             new_database=true
             new_database_name="${2}"
-            shift 2
-            ;;
-        -g|--grant-user)
-            grant_user=true
+            shift 2 ;;
+           --ensure-role)
+            ensure_role=true
+            role_name="${2}"
+            shift 2 ;;
+           --ensure-role-grants)
+            ensure_role_grants=true
             grant_arr=(${2//,/ })
-            shift 2
-            ;;
+            shift 2 ;;
+           --assign-role-to-user)
+            assign_role=true
+            assign_arr=(${2//,/ })
+            shift 2 ;;
         -v|--verbose)
             verbose=true
-            shift
-            ;;
+            shift ;;
         --)
             shift
             options="${@}"
-            break
-            ;;
+            break ;;
         *)
             >&2 echo "Programming error"
-            exit 3
-            ;;
+            exit 3 ;;
     esac
 done
 
@@ -128,18 +133,32 @@ if $new_database; then
     ${admin_bin} db create --name "${new_database_name}" --passwd "${admin_pw}" ${options}
 fi
 
-if $new_user; then
-    log "Adding user ${new_user_name} to stardog"
-    ${admin_bin} user add --new-password $(cat "${new_user_pass_from}") --passwd "${admin_pw}" "${new_user_name}" || \
+if $ensure_user; then
+    log "Adding user ${user_name} to stardog"
+    ${admin_bin} user add --new-password $(cat "${user_pass_from}") --passwd "${admin_pw}" "${user_name}" ${options} || \
     log "User already exists - updating password" && \
-    ${admin_bin} user passwd --new-password $(cat "${new_user_pass_from}") --passwd "${admin_pw}" "${new_user_name}"
+    ${admin_bin} user passwd --new-password $(cat "${user_pass_from}") --passwd "${admin_pw}" "${user_name}" ${options}
 fi
 
-if $grant_user; then
-    user_name=${grant_arr[0]}
+if $ensure_role; then
+    add_command="${admin_bin} role add ${role_name} --passwd ${admin_pw}"
+    log "Creating role ${role_name} if not existing"
+    grep -E "already exists|Successfully" - < <(${add_command}) || ${add_command}
+fi
+
+if $ensure_role_grants; then
+    role_name=${grant_arr[0]}
     action_name=${grant_arr[1]}
     grant_object=${grant_arr[2]}
-    command="${admin_bin} user grant ${user_name} --passwd \"${admin_pw}\" --action \"${action_name}\" --object \"${grant_object}\" \"${user_name}\""
-    log "Granting user '${user_name}' ${action_name} to ${grant_object}"
-    grep 'already has' - < <(${command}) || ${command}
+    log "Granting '${action_name}' permission to role '${role_name}' for '${grant_object}'"
+    grant_command="${admin_bin} role grant --passwd ${admin_pw} --action ${action_name} --object ${grant_object} ${role_name}"
+    grep -E "already has|Successfully" - < <(${grant_command}) || ${grant_command}
+fi
+
+if $assign_role; then
+    role_name=${assign_arr[0]}
+    user_name=${assign_arr[1]}
+    assign_command="${admin_bin} user addrole --role ${role_name} --passwd ${admin_pw} ${user_name}"
+    log "Assigning role ${role_name} to user ${user_name}"
+    grep -E "already has|Successfully" - < <(${assign_command}) || ${assign_command}
 fi
