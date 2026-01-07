appuio/charts

commit 020cc6f4f7c7e33e1ef6027a4456b26710f694f0

stardog: also make keep_firing_for configurable for alerts
See the reference here: https://prometheus.io/docs/prometheus/latest/configuration/recording_rules/#rule
and https://prometheus.io/docs/prometheus/latest/configuration/alerting_rules/#defining-alerting-rules

Issue: SBAR-1501

appuio/stardog/templates/monitoring/stardog-rules.yaml
@@ -17,7 +17,12 @@ spec:
     rules:
     - alert: StardogOpenConnections
       expr: databases_openConnections{ {{ $ns_selector }} } > 32
-      for: {{ default "1m" (((.Values.alerts).stardogOpenConnections).for) | quote }}
+      {{- with .Values.alerts.stardogOpenConnections }}
+      for: {{ default "1m" .for | quote }}
+      {{- if .keep_firing_for }}
+      keep_firing_for: {{ .keep_firing_for | quote }}
+      {{ end -}}
+      {{ end -}}
       annotations:
         summary: Too many open connections
         description: Open connections for DB {{ "{{" }} $labels.database {{ "}}" }} on instance {{ "{{" }} $labels.pod {{ "}}" }} exeeded 32 ({{ "{{" }} $value {{ "}}" }}).
@@ -27,7 +32,12 @@ spec:
         severity: critical
     - alert: StardogLicenseExpire
       expr: min(dbms_license_expiration{ {{ $ns_selector }} }) without (instance, pod) < 42
-      for: {{ default "10m" (((.Values.alerts).stardogLicenseExpire).for) | quote }}
+      {{- with .Values.alerts.stardogLicenseExpire }}
+      for: {{ default "10m" .for | quote }}
+      {{- if .keep_firing_for }}
+      keep_firing_for: {{ .keep_firing_for | quote }}
+      {{ end -}}
+      {{ end -}}
       annotations:
         summary: Stardog license about to expire
         description: The Stardog license will expire in {{ "{{" }} $value {{ "}}" }} days.
@@ -37,7 +47,12 @@ spec:
         severity: "{{ "{{ if lt $value 21.0 }}" }}critical{{ "{{ else }}" }}warning{{ "{{ end }}" }}"
     - alert: StardogPodsNotReady
       expr: kube_statefulset_status_replicas_ready{ {{ $ns_selector }}, statefulset="{{ include "stardog.fullname" . }}" } != {{ .Values.replicaCount }}
-      for: {{ default "10m" (((.Values.alerts).stardogPodsNotReady).for) | quote }}
+      {{- with .Values.alerts.stardogPodsNotReady }}
+      for: {{ default "10m" .for | quote }}
+      {{- if .keep_firing_for }}
+      keep_firing_for: {{ .keep_firing_for | quote }}
+      {{ end -}}
+      {{ end -}}
       annotations:
         summary: Pods of Stardog StatefulSet not ready
         description: Only {{ "{{" }} $value {{ "}}" }} of {{ .Values.replicaCount }} Stardog pods are ready in namespace {{ "{{" }} $labels.namespace {{ "}}" }}.
@@ -48,7 +63,12 @@ spec:
     {{- if .Values.ingress.enabled }}
     - alert: HttpCheck
       expr: probe_success{instance="https://{{ .Values.ingress.host }}/admin/healthcheck"} != 1
-      for: {{ default "1m" (((.Values.alerts).httpCheck).for) | quote }}
+      {{- with .Values.alerts.httpCheck }}
+      for: {{ default "1m" .for | quote }}
+      {{- if .keep_firing_for }}
+      keep_firing_for: {{ .keep_firing_for | quote }}
+      {{ end -}}
+      {{ end -}}
       annotations:
         summary: Http check failed
         description: "Http check failed on {{ "{{" }} $labels.instance {{ "}}" }}"
@@ -58,7 +78,12 @@ spec:
         oncall: "{{ "{{ if eq $labels.env \\\"prod\\\" }}" }}true{{ "{{ else }}" }}false{{" {{ end }} "}}"
     - alert: CertExpirySoon
       expr: probe_ssl_earliest_cert_expiry{instance="https://{{ .Values.ingress.host }}/admin/healthcheck"} - time() < 86400 * 30
-      for: {{ default "10m" (((.Values.alerts).certExpirySoon).for) | quote }}
+      {{- with .Values.alerts.certExpirySoon }}
+      for: {{ default "10m" .for | quote }}
+      {{- if .keep_firing_for }}
+      keep_firing_for: {{ .keep_firing_for | quote }}
+      {{ end -}}
+      {{ end -}}
       annotations:
         summary: SSL ertificate expires in less than 30 days
         description: "SSL certificate on {{ "{{" }} $labels.instance {{ "}}" }} expires is in less than 30 days"

appuio/stardog/values.yaml
@@ -102,14 +102,19 @@ metrics:
 alerts:
   stardogOpenConnections:
     for:
+    keep_firing_for:
   stardogLicenseExpire:
     for:
+    keep_firing_for:
   stardogPodsNotReady:
     for:
+    keep_firing_for:
   httpCheck:
     for:
+    keep_firing_for:
   certExpirySoon:
     for:
+    keep_firing_for:
 resources: {}
 
 nodeSelector: {}

commit e89699153bf85e9725849d36b2c1b12360ae8d6d

stardog: fix wrong newline removal in stardog-rules.yaml
This fixes a bug introduced in commit 020cc6f.

appuio/stardog/Chart.yaml
@@ -1,6 +1,6 @@
 apiVersion: v1
 name: stardog
-version: 0.26.2-2
+version: 0.26.2-3
 appVersion: 10.1.0
 description: Stardog is the world’s leading knowledge graph platform for the enterprise. Stardog makes it fast and easy to turn enterprise data into knowledge.
 home: "https://www.stardog.com/"

appuio/stardog/templates/monitoring/stardog-rules.yaml
@@ -21,8 +21,8 @@ spec:
       for: {{ default "1m" .for | quote }}
       {{- if .keep_firing_for }}
       keep_firing_for: {{ .keep_firing_for | quote }}
-      {{ end -}}
-      {{ end -}}
+      {{- end }}
+      {{- end }}
       annotations:
         summary: Too many open connections
         description: Open connections for DB {{ "{{" }} $labels.database {{ "}}" }} on instance {{ "{{" }} $labels.pod {{ "}}" }} exeeded 32 ({{ "{{" }} $value {{ "}}" }}).
@@ -36,8 +36,8 @@ spec:
       for: {{ default "10m" .for | quote }}
       {{- if .keep_firing_for }}
       keep_firing_for: {{ .keep_firing_for | quote }}
-      {{ end -}}
-      {{ end -}}
+      {{- end }}
+      {{- end }}
       annotations:
         summary: Stardog license about to expire
         description: The Stardog license will expire in {{ "{{" }} $value {{ "}}" }} days.
@@ -51,8 +51,8 @@ spec:
       for: {{ default "10m" .for | quote }}
       {{- if .keep_firing_for }}
       keep_firing_for: {{ .keep_firing_for | quote }}
-      {{ end -}}
-      {{ end -}}
+      {{- end }}
+      {{- end }}
       annotations:
         summary: Pods of Stardog StatefulSet not ready
         description: Only {{ "{{" }} $value {{ "}}" }} of {{ .Values.replicaCount }} Stardog pods are ready in namespace {{ "{{" }} $labels.namespace {{ "}}" }}.
@@ -67,8 +67,8 @@ spec:
       for: {{ default "1m" .for | quote }}
       {{- if .keep_firing_for }}
       keep_firing_for: {{ .keep_firing_for | quote }}
-      {{ end -}}
-      {{ end -}}
+      {{- end }}
+      {{- end }}
       annotations:
         summary: Http check failed
         description: "Http check failed on {{ "{{" }} $labels.instance {{ "}}" }}"
@@ -82,8 +82,8 @@ spec:
       for: {{ default "10m" .for | quote }}
       {{- if .keep_firing_for }}
       keep_firing_for: {{ .keep_firing_for | quote }}
-      {{ end -}}
-      {{ end -}}
+      {{- end }}
+      {{- end }}
       annotations:
         summary: SSL ertificate expires in less than 30 days
         description: "SSL certificate on {{ "{{" }} $labels.instance {{ "}}" }} expires is in less than 30 days"

appuio/stardog/test/main_test.go
@@ -1,7 +1,25 @@
 package test
 
 var (
-	helmChartPath = ".."
-	adminPassword = "something"
-	releaseName   = "test-release"
+	helmChartPath                 = ".."
+	adminPassword                 = "something"
+	releaseName                   = "test-release"
+	minimalOptionsForAllTemplates = map[string]string{
+		"image.password":                   "imagePullSecretPassword",
+		"ingress.enabled":                  "true",
+		"metrics.enabled":                  "true",
+		"metrics.prometheusOperator":       "true",
+		"stardog.adminPassword":            adminPassword,
+		"stardog.backup.databases[0].name": "test",
+		"stardog.backup.s3AccessKey":       "s3AccessKey",
+		"stardog.backup.s3BucketName":      "s3BucketName",
+		"stardog.backup.s3SecretKey":       "s3SecretKey",
+	}
 )
+
+func Merge[K comparable, V any](dst map[K]V, src map[K]V) map[K]V {
+	for k, v := range src {
+		dst[k] = v
+	}
+	return dst
+}

appuio/stardog/test/smoke_test.go
@@ -1,0 +1,67 @@
+package test
+
+import (
+	"io/fs"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Chart_can_be_rendered(t *testing.T) {
+	options := &helm.Options{
+		SetValues: minimalOptionsForAllTemplates,
+	}
+
+	err := renderChart(t, options)
+
+	assert.Nil(t, err)
+}
+
+func Test_Chart_can_be_rendered_with_keep_firing_for(t *testing.T) {
+	options := &helm.Options{
+		SetValues: Merge(minimalOptionsForAllTemplates, map[string]string{
+			"alerts.stardogOpenConnections.keep_firing_for": "1h",
+		}),
+	}
+
+	err := renderChart(t, options)
+
+	assert.Nil(t, err)
+}
+
+func renderChart(t *testing.T, options *helm.Options) error {
+	helmChartPathAbsPath, err := filepath.Abs(helmChartPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	files, err := findYamlFiles(helmChartPathAbsPath+"/templates", helmChartPathAbsPath+"/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = helm.RenderTemplateE(t, options, helmChartPath, releaseName, files)
+	return err
+}
+
+func findYamlFiles(root string, relativeTo string) ([]string, error) {
+	var files []string
+
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() {
+			ext := strings.ToLower(filepath.Ext(path))
+			if ext == ".yaml" || ext == ".yml" {
+				files = append(files, strings.Replace(path, relativeTo, "", 1))
+			}
+		}
+		return nil
+	})
+
+	return files, err
+}